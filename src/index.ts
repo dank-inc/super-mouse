07ffc0a4646e5b8f504e1929f869c73cbae02e17
Elijah <elijahlucian@gmail.com>
2025-01-04 11:58:11 -0700

    mo handlers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 export type SuperMouseParams = {
   element: HTMLElement
-  logging?: boolean
+  debug?: boolean
   disableContext?: boolean
+  onClick?: (e: MouseEvent) => void
+  onMove?: (e: MouseEvent) => void
+  onRelease?: (e: MouseEvent) => void
+  onScroll?: (e: WheelEvent) => void
+  onEnter?: () => void
+  onLeave?: () => void
 }
 
 type MouseButtons = Record<number, boolean>
@@ -16,8 +22,15 @@ export interface SuperMouse {
   scrollY: number
   inertia: number
 
+  onClick?: (e: MouseEvent) => void
+  onMove?: (e: MouseEvent) => void
+  onRelease?: (e: MouseEvent) => void
+  onScroll?: (e: WheelEvent) => void
+  onEnter?: () => void
+  onLeave?: () => void
+
   started: boolean
-  logging: boolean
+  debug: boolean
   dragging: boolean
   buttons: MouseButtons
   keys: KeyMap
@@ -27,7 +40,17 @@ export interface SuperMouse {
 }
 
 export class SuperMouse {
-  constructor({ logging, disableContext, element }: SuperMouseParams) {
+  constructor({
+    debug,
+    disableContext,
+    element,
+    onClick,
+    onMove,
+    onRelease,
+    onScroll,
+    onEnter,
+    onLeave,
+  }: SuperMouseParams) {
     // position
     this.x = 0
     this.y = 0
@@ -37,40 +60,47 @@ export class SuperMouse {
     // scroll behavior
     this.scrollX = 0
     this.scrollY = 0
-    
+
     // phyiscs
     this.inertia = 0
-    
+
     this.started = false
-    this.logging = !!logging
+    this.debug = !!debug
     this.dragging = false
     this.buttons = {}
     this.keys = {}
-    
+
     this.onElement = false
     this.element = element
 
     // if element is canvas type, warn about no key events and how to fix
     console.log(this.element)
 
+    this.onClick = onClick
+    this.onMove = onMove
+    this.onRelease = onRelease
+    this.onScroll = onScroll
+    this.onEnter = onEnter
+    this.onLeave = onLeave
+
     // keydown => mapping object
     // keyup => mapping object
 
     // or not
-    this.element.addEventListener('keydown', e => this.keys[e.key] = true)
-    this.element.addEventListener('keyup', e => this.keys[e.key] = true)
+    this.element.addEventListener("keydown", (e) => (this.keys[e.key] = true))
+    this.element.addEventListener("keyup", (e) => (this.keys[e.key] = true))
 
     this.element.addEventListener("mousedown", this.handleClick)
     this.element.addEventListener("mousemove", this.handleMove)
     this.element.addEventListener("mouseup", this.handleRelease)
     this.element.addEventListener("wheel", this.handleScroll)
 
-    this.element.addEventListener('mouseenter', () => this.onElement = true)
-    this.element.addEventListener('mouseleave', () => this.onElement = false)
-    
-    this.element.addEventListener('doubleclick', () => {})
+    this.element.addEventListener("mouseenter", () => (this.onElement = true))
+    this.element.addEventListener("mouseleave", () => (this.onElement = false))
 
-    if(disableContext) 
+    this.element.addEventListener("doubleclick", () => {})
+
+    if (disableContext)
       this.element.addEventListener("contextmenu", (e) => e.preventDefault())
   }
 
@@ -84,11 +114,16 @@ export class SuperMouse {
 
   handleClick = (e: MouseEvent) => {
     this.buttons[e.button] = true
-    console.log("SuperMouse.click =>", e, this)
+    if (this.debug) console.log("SuperMouse.click =>", e, this)
+
+    this.onClick?.(e)
   }
 
   handleRelease = (e: MouseEvent) => {
     this.buttons[e.button] = false
+    if (this.debug) console.log("SuperMouse.release =>", e, this)
+
+    this.onRelease?.(e)
   }
 
   handleScroll = (e: WheelEvent) => {
@@ -97,12 +132,12 @@ export class SuperMouse {
 
     this.scrollX += e.deltaX * -1
     this.scrollY += e.deltaY * -1
-    console.log("SuperMouse.scroll", this.scrollX, this.scrollY)
+    if (this.debug) console.log("SuperMouse.scroll", this.scrollX, this.scrollY)
+
+    this.onScroll?.(e)
   }
 
   handleMove = (e: MouseEvent) => {
-    
-
     const lastU = this.u
     const lastV = this.v
 
@@ -119,9 +154,15 @@ export class SuperMouse {
 
     const force = (lastU - this.u) ** 2 + (lastV - this.v) ** 2
     this.inertia += force * 50
+
+    if (this.debug) console.log("SuperMouse.move", this.u, this.v)
+
+    this.onMove?.(e)
   }
 
   update = () => {
     this.inertia *= 0.97
+
+    if (this.debug) console.log("SuperMouse.update", this.inertia)
   }
 }
